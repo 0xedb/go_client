752536bef0a89c3f42eb8049fad019859c1d2216
0xedb <edohbruno@gmail.com>
2021-07-02 16:46:42 +0000

    add goroutines to server

--- client/client.go
+++ client/client.go
@@ -1,50 +1,49 @@
 package client
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 	"net/http"
+	"sync"
 	"time"
 )
 
 func getClient() *http.Client {
 	client := &http.Client{}
 
-	// client.
-	client.Timeout = 10 * time.Second
+	client.Timeout = 6 * time.Second
 
 	return client
 }
 
 func MakeRequest(url string) {
 	var client = getClient()
-
-	// client.Do(
+ 
 	res, err := client.Get(url)
 	fmt.Println("making request...")
-
-	fmt.Println("TIMEOUT", client.Timeout)
 
 	if err != nil {
 		log.Fatal("unable to make request ", err)
 	}
 
-	answer := []byte{}
-	res.Body.Read(answer)
-	fmt.Println(string(answer))
+	answer := bytes.Buffer{}
+	answer.ReadFrom(res.Body)
+	fmt.Println("got ", answer.String())
 }
 
 func getServer() *http.Server {
 	return &http.Server{Addr: ":2021"}
 }
 
-func StartServer() {
+func StartServer(wg *sync.WaitGroup) {
+	defer wg.Done()
 	server := getServer()
 
 	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
-		time.Sleep(10 * time.Second)
+		time.Sleep(2 * time.Second)
 		res.Header().Add("Content-Type", "application/json")
-		res.Write([]byte("{\"msg\":\"goodbye\"}"))
+		res.Write([]byte("{\"msg\":\"BYE!!!\"}"))
 	})
 
 	fmt.Println("starting server on", server.Addr)
--- cmd/client.go
+++ cmd/client.go
@@ -2,12 +2,25 @@ package main
 
 import (
 	"fmt"
+	"sync"
+	"time"
 
 	"github.com/0xedb/go_client/client"
 )
 
 func main() {
+	url := "http://localhost:2021/"
 	fmt.Println("hello")
 
-	client.StartServer()
+	var wg sync.WaitGroup
+
+	time.AfterFunc(2*time.Second, func() {
+
+		client.MakeRequest(url)
+	})
+
+	wg.Add(1)
+	go client.StartServer(&wg)
+
+	wg.Wait()
 }
